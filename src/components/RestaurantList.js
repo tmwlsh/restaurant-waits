53f7911c9aaa1aed30df7e8dffd2392af854d7ed
Tom Walsh <tom.walsh@homeagency.co.uk>
2019-11-20 17:08:08 +0000

    adds website to restaurants

--- src/components/RestaurantList.js
+++ src/components/RestaurantList.js
@@ -1,4 +1,5 @@
 import React from "react"
+import { Link } from "gatsby"
 import Data from "../data.json"
 
 const RestaurantList = () => {
@@ -13,8 +14,11 @@ const RestaurantList = () => {
             <li>
               <div className="inner">
                 <h3>{restaurants[entry].name}</h3>
-                <p>{restaurants[entry].location}</p>
-                <p>Wait Time: {restaurants[entry].wait}</p>
+                <p><span>Location</span>: {restaurants[entry].location}</p>
+                <p><span>Wait Time</span>: {restaurants[entry].wait}</p>
+                <a target="_blank" href={restaurants[entry].website}>
+                  <span>Go To Website</span>
+                </a>
               </div>
             </li>
           )
